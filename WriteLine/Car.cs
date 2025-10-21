f3d081303f921779d588ff8845cd88c7f57aaa6e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-21 14:48:12 +0330

    decler type

---
+++ WriteLine/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    class Car
+    {
+        public string CarName;
+        public string CarModel;
+        public int year;
+
+    }
+}
