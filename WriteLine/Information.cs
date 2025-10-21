5bea5c6bc4c65ff686008c429e51ded46625550e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-21 14:20:09 +0330

    add information(name and family)

---
+++ WriteLine/Information.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    class Information
+    {
+        public static string fullName(string name, string family)
+        {
+            return name + " " + family;
+        }
+    }
+}
