811aed64b25d0245ad60f4d00cc333659af8f942
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-22 10:22:46 +0330

    add static because should inherit

---
+++ WriteLine/Parent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    class Parent
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
