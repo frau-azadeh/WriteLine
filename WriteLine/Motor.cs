eb8c2ee58abfcb812c382be73490b99def7c150a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-22 13:04:53 +0330

    add class

---
+++ WriteLine/Motor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    class Motor
+    {
+        private int _speed;
+
+        public int Speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                if (value > 120)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("your ride very fast!");
+                    _speed = 0;
+                    Console.ResetColor();
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+    }
+}
