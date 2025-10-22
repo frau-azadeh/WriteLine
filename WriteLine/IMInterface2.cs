3307e69637d13bf8b5f302577293da9d15241716
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-22 15:23:39 +0330

    add inheritance for admin

---
+++ WriteLine/IMInterface2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    interface IMInterface2
+    {
+        string HelloAdmin(string name);
+    }
+}
