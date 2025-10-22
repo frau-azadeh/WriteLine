bde8b29ecf533ac53ecae9ba29443b0b543b14e7
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-10-22 15:51:38 +0330

    add person

---
+++ WriteLine/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WriteLine
+{
+    class Person
+    {
+        public string Name {
+            get;
+            set;
+        }
+
+        public string Family
+        {
+            get;
+            set;
+        }
+
+        public int Age
+        {
+            get;
+            set;
+        }
+    }
+}
